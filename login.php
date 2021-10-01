48fa313ff22fcdb5df456eee53337ffb3a031f3f
TENSHI101 <48865498+garciarodrigue@users.noreply.github.com>
2021-10-01 15:21:01 -0600

    Initial commit

---
+++ login.php
@@ -0,0 +1,4 @@
+<?php
+$conexion=mysqli_connect("root","localhost", "1011175","login");
+
+?>
---
+++ validar.php
@@ -0,0 +1,29 @@
+<?php
+$usuario= $_POST['usuario'];
+$password= $_POST['password'];
+session_start();
+$_SESSION['usuario'] = $usuario;
+
+include('login.php');
+
+//$conexion=mysqli_connect('localhost','tenshi', ' ', 'login' );
+
+$consulta = "SELECT*FROM login WHERE login ='$usuario' and password = '$password' ";
+
+$resultado=mysqli_query($conexion, $consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+	header("location:home.html");
+}else{
+	?>
+	<?php 
+	include("index.html");
+	?>
+	<h1 class="bad">Error</h1>
+	<?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
+?>
